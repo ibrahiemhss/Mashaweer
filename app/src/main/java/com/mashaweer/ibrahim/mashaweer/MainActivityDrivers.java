67a599b0eb84df6dba174c837e0354668188c469
ibrahim <kekoreko3@gmail.com>
2018-01-31 13:31:20 +0200

    1

--- app/src/main/java/com/mashaweer/ibrahim/mashaweer/MainActivityDrivers.java
+++ app/src/main/java/com/mashaweer/ibrahim/mashaweer/MainActivityDrivers.java
@@ -46,6 +46,7 @@ public class MainActivityDrivers extends AppCompatActivity
     private RecyclerView Desplay_Shofires_For_Clients;
     private MyDrawerLayout mDrawerLayout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
